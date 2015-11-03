245a9856b84386a9e50024014626610eae40b7c9
dennyseptianto <septiantodenny@gmail.com>
2015-11-03 14:09:43 +0700

    pi
    pi

---
+++ contact.php
@@ -0,0 +1,19 @@
+<div class="header-banner" align="center">
+					<!-- Top Navigation -->
+					<section class="bgi banner5"><h2>Contact</h2> </section>
+					<!-- untuk judul halaman -->
+		<div class="typrography">
+	 <div class="container">
+				<article class="isi">
+				<p>Jika pertanyaan lebih lanjut, silahkan hubungi admin dibawah ini :</p>
+					<table>
+						<tr><td style="width:100px;">Nama<td style="width:10px;">:<td>Denny Septianto (A11.2012.07110)</tr>
+						<tr><td>		<td><td>Yuni Arief Setyo   (A11.2012.06836)</tr>
+						<tr><td>		<td><td>Aldhi Ari Kurniawan (A11.2013.07580)</tr>
+						<tr><td>Email		<td>:<td> septiantodenny@gmail.com</tr>
+					</table>
+				</article>
+				<!-- isi penjelasan halaman -->
+			</div>
+		</div>
+	</div>
