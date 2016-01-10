79c72aac2ca235f87edf890d48f70841c2a81b98
dennyseptianto <septiantodenny@gmail.com>
2016-01-10 16:23:42 +0700

    register.php

--- register.php
+++ register.php
@@ -1,19 +1,30 @@
-			<div id="main">
-				<h2 class="isi">Daftar Baru</h2>
-				<!-- untuk judul halaman -->
-				<article class="isi">
-					<div class="register">
-						<form name="daftar" method="post" action="">
-							<table>
-							<td>Username <td>: <input id="id" type="text" name="id" value="" placeholder="Username" required><tr>
-							<td>Nama Lengkap <td>: <input id="nama" type="text" name="nama" value="" placeholder="Nama Lengkap" required><tr>
-							<td>Password <td>: <input id="password" type="password" name="password" value="" placeholder="Password" required><tr>
-							<td>Email <td>: <input id="email" type="email" name="email" value="" placeholder="Email" required><tr>
-							<td>Tanggal Lahir <td>: <input type="text" id="datepicker" name="tgl_lahir" value="" placeholder="Tanggal Lahir" required><tr>
-							<td><p class="submit"><input type="submit" name="Daftar" value="Daftar"> <button type="reset" class="cancel">Cancel</button></p><tr>
-							</table>
-						</form>
-					</div>
-				</article>
-				<!-- isi penjelasan halaman -->
-			</div>
+<?php
+									if (isset($_SESSION['username']))
+									{
+										if($_SESSION['username'] || isset($_SESSION['username'])) {
+											echo '<a href="index.php?page=tes"><button type="submit" class="btn btn-info mrgn-mulai">Mulai Test</button> </a>';
+										}
+									}
+									else {	
+										
+										?>
+											<div class="sign">Create Your Account</div>
+											<div class="banner-forms">
+												<form name="daftar" method="post" action="">
+                                                	
+													<input class="name" type="text" name="id" value="" placeholder="Username" required>
+													<input class="mail2" type="text" name="nama" value="" placeholder="Nama Lengkap" required >
+													<input class="mail2" type="password" name="password" value="" placeholder="Password" required >
+													<input class="mail2" type="email" name="email" value="" placeholder="Email" required>
+													<input class="mail2" type="text" id="datepicker" name="tgl_lahir" value="" placeholder="Tanggal Lahir" required >
+													
+														
+														
+													<button type="submit" name="Daftar" value="Daftar" class="btn btn-info mrgn-can">Sign Up</button>
+                                                    <div class="emailservice">or Sign Up with :</div>
+                                                    <button type="submit" class="btn btn-info mrgn-can2">+Google</button>
+                                                    <button type="submit" class="btn btn-info mrgn-can3">Facebook</button>
+												</form>
+											</div><?php
+									}
+								?>
