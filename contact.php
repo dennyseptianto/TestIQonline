9cc8e2ad0d79a049bf3cb1e586cc06a5e1a8348e
dennyseptianto <septiantodenny@gmail.com>
2016-01-10 16:32:29 +0700

    contact.php

--- contact.php
+++ contact.php
@@ -1,17 +1,35 @@
-<div class="header-banner" align="center">
+	<div class="header-banner" align="center">
 					<!-- Top Navigation -->
 					<section class="bgi banner5"><h2>Contact</h2> </section>
 					<!-- untuk judul halaman -->
 		<div class="typrography">
 	 <div class="container">
 				<article class="isi">
-				<p>Jika pertanyaan lebih lanjut, silahkan hubungi admin dibawah ini :</p>
-					<table>
+				<?php include "map.php";?>
+				
+			<div class="mail-grids">
+				<div class="col-md-6 mail-grid-left">
+					<h3>Kelompok</h3>
+					<h4>Jika pertanyaan lebih lanjut, silahkan hubungi admin dibawah ini :</h4>
+					<h5><table>
 						<tr><td style="width:100px;">Nama<td style="width:10px;">:<td>Denny Septianto (A11.2012.07110)</tr>
 						<tr><td>		<td><td>Yuni Arief Setyo   (A11.2012.06836)</tr>
 						<tr><td>		<td><td>Aldhi Ari Kurniawan (A11.2013.07580)</tr>
-						<tr><td>Email		<td>:<td> septiantodenny@gmail.com</tr>
-					</table>
+						<tr><td>Email		<td>:<td> <a href="mailto:info@example.com">septiantodenny@gmail.com</a></tr>
+					</table></h5>
+					
+				</div>
+				<div class="col-md-6 contact-form">
+					<form name="kirim_psn" method="post" action="">
+						<input type="text" name="nama" placeholder="Nama" required="">
+						<input type="text" name="email" placeholder="Email" required="">
+						<input type="text" name="subjek" placeholder="Subjek" required="">
+						<textarea name="pesan" placeholder="Pesan" required=""></textarea>
+						<input type="submit" name="kirim_psn" value="SEND">
+					</form>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
 				</article>
 				<!-- isi penjelasan halaman -->
 			</div>
